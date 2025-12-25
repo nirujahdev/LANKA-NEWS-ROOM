b7390001953e493ce1a666363452a47af6b182c5
Benaiah <nathanbenaiah4@gmail.com>
2025-12-25 23:50:24 +0530

    Update Google Analytics script to match exact gtag.js implementation

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -17,16 +17,14 @@ export default function GoogleAnalytics() {
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
+        async
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}', {
-            'anonymize_ip': true,
-            'cookie_flags': 'SameSite=None;Secure'
-          });
+          gtag('config', '${GA_MEASUREMENT_ID}');
         `}
       </Script>
     </>
